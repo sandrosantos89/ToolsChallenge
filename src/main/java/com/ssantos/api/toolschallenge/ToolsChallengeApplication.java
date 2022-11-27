5c465ba2574c6b1d6deddc154eda4e2fd502f61d
Sandro Santos <sandro.ads.dev@gmail.com>
2022-11-27 08:40:51 -0300

    Primeiro commit

---
+++ src/main/java/com/ssantos/api/toolschallenge/ToolsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ssantos.api.toolschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssantos/api/toolschallenge/ToolsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssantos.api.toolschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
